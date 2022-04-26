58b9a48affb6499e35dd4506e89e5b1ae603b27e
Mads Steentoft <mstee20@student.aau.dk>
2022-04-26 08:35:19 +0200

    wifi things

--- Kode/Edgenode/WiFi/WiFi.py
+++ Kode/Edgenode/WiFi/WiFi.py
@@ -13,14 +13,14 @@ VIDEO_PORT = 2025
 Command_PORT = 2024
 
 # Addresses
-Edgenode_ADDR = (Edge_IP, VIDEO_PORT)
-Sentry_ADDR = (Sentry_IP, Command_PORT)
+Edgenode_ADDR = (Edge_IP, VIDEO_PORT)   # Edgenode address
+Sentry_ADDR = (Sentry_IP, Command_PORT) # Sentry unit address
 
 # Setup the sockets
 video_udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # Find local ip and Input ip and socket into video_udp
 video_udp.bind(Edgenode_ADDR) # Bind socket to ip and port.
-video_udp.settimeout(0.2)
-command_udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+video_udp.settimeout(2) # set timeout for the receiving socket such that it gives an error if exceeded.
+command_udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # Create socket for commands
 
 #_________________________________________________________________________________________#
 #_____________________________________functions___________________________________________#
@@ -28,14 +28,20 @@ command_udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 # Decode video
 def Decoder():
-    print("Decoding") # decode video feed
+    print("Decoding") # Decode video feed
 
-# Receive videofeed
-def Receive(BUFFER_SIZE):
+
+def Receive(BUFFER_SIZE): # Receive videofeed udp
     data, addr = video_udp.recvfrom(BUFFER_SIZE) # recieve feed from Sentry unit 
 
     return data
 
-# Send Commands
-def Send(commands):
-    command_udp.sendto(commands, Sentry_ADDR)
+
+def Send(commands): # UDP communication for commands
+    command_udp.sendto(commands, Sentry_ADDR) 
+
+
+def tcpSend(commands): # TCP communication for commands
+    command_udp.connect(Sentry_ADDR)
+    command_udp.send(commands)
+    command_udp.close()
--- Kode/Edgenode/run.py
+++ Kode/Edgenode/run.py
@@ -2,11 +2,24 @@ import WiFi.WiFi
 import multiprocessing
 import socket
 import time
+import struct
+import numpy as np
+import math
 
 net = WiFi.WiFi
 
 data = b''
-msg = b'0,0,0'
+
+yawAngle = 0
+yawSpeed = 0
+pitchAngle = 0 
+pitchSpeed = 0
+
+
+def packer():
+    struct.pack(h, yawAngle, pitchangle)
+    struct.pack(B, yawSpeed,pitchSpeed)
+
 
 def Receiver(BUFFER_SIZE):
     while(1):
@@ -14,15 +27,21 @@ def Receiver(BUFFER_SIZE):
             data = net.Receive(BUFFER_SIZE)
         except:
             print("Didn't receive anything")
-      
+        print(data)   
 
 def Sender(commands):
     while(1):
         print("sending commands")
         net.Send(commands)
 
+def tcpSender(commands):
+    while(1):
+        print("sending commands")
+        net.tcpSend(commands)
+
+
 if __name__ == '__main__':
-    sendProcess = multiprocessing.Process(target=Sender, args=(msg,))
+    sendProcess = multiprocessing.Process(target=Sender, args=(commands,))
     receiveProcess = multiprocessing.Process(target=Receiver, args=(2048,))
 
     receiveProcess.start()
