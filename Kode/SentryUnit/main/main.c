f33e27852e633fb6d26b711e4d6ba56cc2c0ea8c
c3lphie <c3lphie@protonmail.com>
2022-05-11 10:23:13 +0200

    Can now connect to wifi

--- Kode/SentryUnit/main/main.c
+++ Kode/SentryUnit/main/main.c
@@ -1,9 +1,19 @@
 /* Standard libraries */
 #include <stdio.h>
 
+#include "lwip/sockets.h"
 #include "wifi.h"
+#include "nvs_flash.h"
 
 void app_main(void)
 {
+  esp_err_t ret = nvs_flash_init();
+  if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+    ESP_ERROR_CHECK(nvs_flash_erase());
+    ret = nvs_flash_init();
+  }
 
+  ESP_ERROR_CHECK(ret);
+
+  connectWifi();
 }
