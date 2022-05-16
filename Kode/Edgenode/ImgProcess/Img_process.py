fbf4c3e0f9f020b2dda85a812c3d28a231e0e0a4
Anders H <andersdane@live.dk>
2022-05-16 14:46:51 +0200

    test update

--- Kode/Edgenode/ImgProcess/Img_process.py
+++ Kode/Edgenode/ImgProcess/Img_process.py
@@ -32,34 +32,34 @@ def drone_detection(feed):
 
         # Convert frame to HSV
         hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-        print("1") # Statement control
+        #print("1") # Statement control
 
         # Turn color range white and the rest black
         mask = cv2.inRange(hsv, lower_color, upper_color)
-        print("2") # Statement control
+        #print("2") # Statement control
 
         # Calculate frame center
         frame_height_center = height/2
         frame_width_center = width/2
-        print("3") # Statement control
+        #print("3") # Statement control
 
         # Reduce the noise on frame with kernel
         opening = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
-        print("4") # Statement control
+        #print("4") # Statement control
 
         # Extract coordinates, width and height of 
         x, y, w, h = cv2.boundingRect(opening)
-        print("5") # Statement control
+        #print("5") # Statement control
 
         # Calculate drone center
         drone_height_center = int(y+h/2)
         drone_width_center = int(x+w/2)
-        print("6") # Statement control
+        #print("6") # Statement control
 
         # Create rectangle and center dot
         cv2.rectangle(frame, (x, y), (x+w, y + h), (0, 255, 0), 3)
         cv2.circle(frame, (drone_width_center, drone_height_center), 5, (0, 0, 255), -1)
-        print("7") # Statement control
+        #print("7") # Statement control
         
         cv2.imshow('Color detection', frame) # Display video with rectangle and center dot
 
@@ -68,7 +68,7 @@ def drone_detection(feed):
         if key == ord('q'):
             return
         
-        print("8") # Statement control 
+        #print("8") # Statement control 
         return(frame_height_center, frame_width_center, drone_width_center, drone_height_center)
 
     except:
